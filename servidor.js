7ff4b86e273a8c02bd8781385816322ec394d188
Douguinhapi <douguinhapi123@gmail.com>
2024-09-16 17:33:48 -0300

    dff

--- servidor.js
+++ servidor.js
@@ -4,16 +4,39 @@ const app = express()
 const path = require("path")
 
 const PORTA = 3000
-//Avisar ao expresse que os nossos arquivos estáticos estão aqui para ele juntar ao html e enviar
-//Na pasta public entra CSS, imagem, icones, JS, ...
+
 app.use(
-    express.static(path.join(__dirname,"public"))
+    express.static(path.join(__dirname, "public"))
 )
-//Rota que o cliente vai acessar e o método que ele vai usar no caso GET
-app.get("/", (pedido,resposta) => {
-    resposta.sendFile(path.join(__dirname,"views","index.html"))
+
+app.get("/", (req, res) => {
+    res.redirect("/bufalo")
 })
 
-app.listen(PORTA, ()=>{
-    console.log(`Servidor rodando com sucesso no  endereço: localhost:${PORTA}`)
+app.get("/bufalo", (req,res) =>{
+    res.sendFile(path.join(__dirname, "views", "bufalo.html"))
+})
+
+app.get("/esquilo", (req,res) =>{
+    res.sendFile(path.join(__dirname, "views", "esquilo.html"))
+})
+
+app.get("/ganso", (req,res) =>{
+    res.sendFile(path.join(__dirname, "views", "ganso.html"))
+})
+
+app.get("/garca", (req,res) =>{
+    res.sendFile(path.join(__dirname, "views", "garca.html"))
+})
+
+app.get("/leao", (req,res) =>{
+    res.sendFile(path.join(__dirname, "views", "leao.html"))
+})
+
+app.get("/tartaruga", (req,res) =>{
+    res.sendFile(path.join(__dirname, "views", "tartaruga.html"))
+})
+
+app.listen(PORTA,()=>{
+    console.log(`Servidor rodando na porta: localhost:${PORTA}`)
 })
